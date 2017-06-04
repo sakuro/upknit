d1f3d02e055e488b0bfa8ce93c9488c29b6d03bb
OZAWA Sakuro <sakuro@2238club.org>
2017-06-04 15:27:56 +0900

    :cop: Fix RSpec/Focus

--- spec/upknit/engine_spec.rb
+++ spec/upknit/engine_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Upknit::Engine do
   describe 'extension' do
-    it 'enables pgcryto extension', focus: true do
+    it 'enables pgcryto extension' do
       expect(ActiveRecord::Base.connection.extensions).to include('pgcrypto')
     end
   end
